aaf5263b877a8e988b5994c55987a7f4b8f24a9a
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-07-18 11:50:22 -0400

    wins and losses text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,20 @@
 var randomNumber = Math.floor(Math.random()*102+19);
 
 $("#number").text(randomNumber);
+
+var pinkCrystal = Math.floor(Math.random()*11+1);
+var blueCrystal = Math.floor(Math.random()*11+1);
+var greenCrystal = Math.floor(Math.random()*11+1);
+var yellowCrystal = Math.floor(Math.random()*11+1);
+
+var wins = 0;
+
+$("#wins").text(wins);
+
+var losses = 0;
+
+$("#losses").text(losses);
+
+var score = 0;
+
+
